914055e70444386ea0d7964909f1649e9d1b979f
macsimir <darknezzz192@gmail.com>
2024-11-13 19:54:09 +0300

    commit

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ async def start_web_app():
         app,
         host="127.0.0.1",  # или 0.0.0.0 для общедоступного доступа
         port=8443,
-        ssl_certfile="cert.pem",  # путь к сертификату
-        ssl_keyfile="key.pem",    # путь к приватному ключу
         log_level="info"
     )
     server = uvicorn.Server(config)
--- web/app.py
+++ web/app.py
@@ -1,8 +1,15 @@
-# web/app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
+# Создаем экземпляр FastAPI
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello from FastAPI!"}
+# Настраиваем Jinja2 для шаблонов
+templates = Jinja2Templates(directory="web/templates")
+
+# Маршрут для отображения HTML
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
