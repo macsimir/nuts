86d277a32928663bfee07f628daf1e162e870cfc
macsimir <darknezzz192@gmail.com>
2024-11-19 19:58:46 +0300

    commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def start_web_app():
     config = uvicorn.Config(
         app,
         host="0.0.0.0",  # или 0.0.0.0 для общедоступного доступа
-        port=9000,
+        port=10000,
         log_level="info"
     )
     server = uvicorn.Server(config)
